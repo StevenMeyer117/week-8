8d6ff1a3e34441893f052fc06979755a62d0bd07
Steven Meyer <steven.a.meyer23@outlook.com>
2026-03-24 10:36:21 -0400

    Commit changes for PEP-8

--- apputil.py
+++ apputil.py
@@ -1,20 +1,31 @@
 from collections import defaultdict
-import random
 import numpy as np
 
 
 class MarkovText(object):
+    """
+    Markov text generator based on a given corpus.
+    """
 
     def __init__(self, corpus):
+        """
+        Initialize the MarkovText object.
+
+        Parameters:
+            corpus (str): Input text used to build the Markov model.
+        """
         self.corpus = corpus
-        self.term_dict = None  # you'll need to build this
+        self.term_dict = None  # will store word transitions
 
     def get_term_dict(self):
+        """
+        Build a dictionary mapping each word to a list of possible next words.
 
-        # your code here ...
-
+        Returns:
+            dict: A dictionary where keys are words and values are lists of
+                  words that follow them in the corpus.
+        """
         words = self.corpus.split()
-
         term_dict = defaultdict(list)
 
         for i in range(len(words) - 1):
@@ -23,14 +34,22 @@ class MarkovText(object):
             term_dict[current_word].append(next_word)
 
         self.term_dict = dict(term_dict)
-
         return self.term_dict
 
-
-    
-
     def generate(self, seed_term=None, term_count=15):
+        """
+        Generate text using a Markov chain.
 
+        Parameters:
+            seed_term (str, optional): Starting word for generation.
+            term_count (int): Number of words to generate.
+
+        Returns:
+            str: Generated text string.
+
+        Raises:
+            ValueError: If the seed_term is not in the corpus.
+        """
         if self.term_dict is None:
             self.get_term_dict()
 
@@ -48,7 +67,6 @@ class MarkovText(object):
         output = [current_word]
 
         for _ in range(term_count - 1):
-
             next_words = self.term_dict.get(current_word)
 
             # Handle edge case (last word or dead end)
