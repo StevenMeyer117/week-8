b5ba470ff496b6a878db74baa21ead65eec21dcf
Steven Meyer <steven.a.meyer23@outlook.com>
2026-03-24 10:33:02 -0400

    Exercise 8 commit

--- apputil.py
+++ apputil.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+import random
+import numpy as np
 
 
 class MarkovText(object):
@@ -11,13 +13,49 @@ class MarkovText(object):
 
         # your code here ...
 
-        self.term_dict = {}
+        words = self.corpus.split()
 
-        return None
+        term_dict = defaultdict(list)
 
+        for i in range(len(words) - 1):
+            current_word = words[i]
+            next_word = words[i + 1]
+            term_dict[current_word].append(next_word)
+
+        self.term_dict = dict(term_dict)
+
+        return self.term_dict
+
+
+    
 
     def generate(self, seed_term=None, term_count=15):
 
-        # your code here ...
+        if self.term_dict is None:
+            self.get_term_dict()
 
-        return None
+        if not self.term_dict:
+            return ""
+
+        # Handle seed term
+        if seed_term is not None:
+            if seed_term not in self.corpus.split():
+                raise ValueError("Seed term not found in corpus.")
+            current_word = seed_term
+        else:
+            current_word = np.random.choice(list(self.term_dict.keys()))
+
+        output = [current_word]
+
+        for _ in range(term_count - 1):
+
+            next_words = self.term_dict.get(current_word)
+
+            # Handle edge case (last word or dead end)
+            if not next_words:
+                break
+
+            current_word = np.random.choice(next_words)
+            output.append(current_word)
+
+        return " ".join(output)
